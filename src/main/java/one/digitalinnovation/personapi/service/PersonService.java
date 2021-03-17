0575798ddf15ab4f9a99823404bfc0d2a702255e
Uildson S. dos Santos <wilder_shon@hotmail.com>
2021-03-16 21:00:42 -0300

    Developed delete by Id operation

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -3,12 +3,14 @@ package one.digitalinnovation.personapi.service;
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -40,5 +42,27 @@ public class PersonService {
         return allPeople.stream()
                 .map(personMapper::toDTO)
                 .collect(Collectors.toList());
+    }
+
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+        Person person = verityIfExists(id);
+        return personMapper.toDTO(person);
+
+//        or
+//        Optional<Person> optionalPerson = personRepository.findById(id);
+//        if (optionalPerson.isEmpty()){
+//            throw new PersonNotFoundException(id);
+//        }
+//        return personMapper.toDTO(optionalPerson.get());
+    }
+
+    private Person verityIfExists(Long id) throws PersonNotFoundException {
+        return personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
+    }
+
+    public void delete(Long id) throws PersonNotFoundException {
+        verityIfExists(id);
+        personRepository.deleteById(id);
     }
 }
